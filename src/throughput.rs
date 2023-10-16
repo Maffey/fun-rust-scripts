bbc4aef087cd922b3a99bb299ab2940ddbab4f83
Mateusz Kuniczuk <matiusz98@gmail.com>
2023-10-16 21:35:16 +0200

    added test for throughput

--- src/throughput.rs
+++ src/throughput.rs
@@ -2,7 +2,7 @@ use std::io;
 use std::process::exit;
 use std::time::Instant;
 
-fn convert_bytes(value: u32, target_units: &str) -> f32 {
+fn convert_bytes(value: u64, target_units: &str) -> f32 {
     match target_units {
         "kilobits" => (value / 125) as f32,
         "kilobytes" => (value / 1_000) as f32,
@@ -15,7 +15,7 @@ fn convert_bytes(value: u32, target_units: &str) -> f32 {
     }
 }
 
-fn process_user_input(args: (u32, &str)) {
+fn process_user_input(args: (u64, &str)) {
     let throughput = convert_bytes(args.0, args.1);
     println!("{} bytes = {} {}", args.0, throughput, args.1);
 }
@@ -35,7 +35,7 @@ pub fn run_throughput_converter() {
 
     let now = Instant::now();
 
-    let user_throughput: u32 = match user_throughput.trim().parse() {
+    let user_throughput: u64 = match user_throughput.trim().parse() {
         Ok(number) => number,
         Err(_) => {
             println!("Incorrect value!");
@@ -58,3 +58,23 @@ pub fn run_throughput_converter() {
 }
 
 //  Performance: 69µs (0.000069 seconds). 14.5 faster than Python.
+
+#[cfg(test)]
+mod tests {
+    use crate::throughput::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(1, "", 1.0)]
+    #[case(1_250, "kilobits", 10.0)]
+    #[case(1_000, "kilobytes", 1.0)]
+    #[case(5_000_000, "megabytes", 5.0)]
+    #[case(10_000_000_000, "gigabytes", 10.0)]
+    fn test_convert_bytes(
+        #[case] value: u64,
+        #[case] target_units: &str,
+        #[case] expected_result: f32,
+    ) {
+        assert_eq!(convert_bytes(value, target_units), expected_result)
+    }
+}
