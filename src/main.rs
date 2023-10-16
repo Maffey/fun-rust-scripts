46aeac3bb0c50973e6e92e76c5c0be259dd0e57b
Mateusz Kuniczuk <matiusz98@gmail.com>
2023-10-16 21:27:01 +0200

    Import throughput script

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use crate::calculate_sequence_easy::run_calculate_sequence_easy;
+use crate::throughput::run_throughput_converter;
 use clap::Parser;
 use std::str::FromStr;
 
 mod calculate_sequence_easy;
+mod throughput;
 
 // TODO show user programs that can be run.
 
@@ -10,6 +12,7 @@ mod calculate_sequence_easy;
 enum Program {
     // TODO: Implement Display instead of debug.
     CalculateSequenceEasy,
+    Throughput,
 }
 
 impl FromStr for Program {
@@ -18,6 +21,9 @@ impl FromStr for Program {
     fn from_str(input: &str) -> Result<Self, Self::Err> {
         match input {
             "calculate_sequence_easy" => Ok(Program::CalculateSequenceEasy),
+            "calculate-sequence-easy" => Ok(Program::CalculateSequenceEasy),
+            "cse" => Ok(Program::CalculateSequenceEasy),
+            "throughput" => Ok(Program::Throughput),
             _ => Err(()),
         }
     }
@@ -42,6 +48,7 @@ fn run_program(program: &Program) {
     println!("Running {:?}...", &program);
     match &program {
         Program::CalculateSequenceEasy => run_calculate_sequence_easy(),
+        Program::Throughput => run_throughput_converter(),
     }
 }
 
